cc509ebf9c0de2b2f52a438f9e09bd9793a003d9
Nantanart Takonuan <nantanart@six.network>
2022-02-08 18:43:26 +0700

    fix: Change Klaytn new logo

--- src/uikit-dev/components/SwitchNetwork.tsx
+++ src/uikit-dev/components/SwitchNetwork.tsx
@@ -3,6 +3,7 @@ import React, { memo, useState } from 'react'
 import styled from 'styled-components'
 import klaytnWhite from '../images/for-ui-v2/toggle-icon/Definix-guide2-03.png'
 import klaytn from '../images/for-ui-v2/toggle-icon/Definix-guide2-04.png'
+import klaytnNewLogo from '../images/for-ui-v2/toggle-icon/Definix-New-Klaytn-Logo.png'
 import bscWhite from '../images/for-ui-v2/toggle-icon/Definix-guide2-05.png'
 import bsc from '../images/for-ui-v2/toggle-icon/Definix-guide2-06.png'
 import { IconButton } from './Button'
@@ -110,7 +111,7 @@ const SwitchNetwork = () => {
           // setIsBsc(false)
         }}
       >
-        <img src={!isBsc || isDark ? klaytnWhite : klaytn} alt="" width="20px" />
+        <img src={!isBsc || isDark ? klaytnNewLogo : klaytnNewLogo} alt="" width="20px" />
         <Tooltip>Klaytn Chain</Tooltip>
       </IconButton>
     </SwitchStyle>
